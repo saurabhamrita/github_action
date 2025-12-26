720d9ab6ce1df27298437b525c7fba02985e92f4
saurabhamrita <sk74564@gmail.com>
2025-12-26 15:49:10 +0530

    testing

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "dhondu" {
+name    = "dhandu-rg"
+location = "East US"  
+}
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+required_providers {
+azurerm = {
+source = "hashicorp/azurerm"        
+version = "4.57.0"  
+}    
+}
+}
+
+provider "azurerm" {
+features{}
+subscription_id = "6e06e212-633e-4e8c-8ab0-7ea51400a2e2"  
+}
